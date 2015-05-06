7b204cdbc34b1090c7df3cb8db9ed426a740a196
EJNolte <enmail@wfelix.de>
2015-05-06 22:20:32 +0200

    Session 06.05.15

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,30 @@
                 when('/', {
                     templateUrl: 'templates/startseite.html'
                 }).
+                when('/termine', {
+                    templateUrl: 'templates/termine.html'
+                }).
+                when('/einsaetze', {
+                    templateUrl: 'templates/einsaetze.html'
+                }).
+                when('/personal', {
+                    templateUrl: 'templates/personal.html'
+                }).
+                when('/ausruestung', {
+                    templateUrl: 'templates/ausruestung.html'
+                }).
+                when('/mitmachen', {
+                    templateUrl: 'templates/mitmachen.html'
+                }).
+                when('/chronik', {
+                    templateUrl: 'templates/chronik.html'
+                }).
+                when('/chronik/geschichte-der-feuerwehr-hackenberg-leienbach', {
+                    templateUrl: 'templates/chronik/geschichte-der-feuerwehr-hackenberg-leienbach.html'
+                }).
+                when('/chronik/liste-bisheriger-zugfuehrer', {
+                    templateUrl: 'templates/chronik/liste-bisheriger-zugfuehrer.html'
+                }).
                 when('/impressum', {
                     templateUrl: 'templates/impressum.html'
                 }).
