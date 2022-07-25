2c163b263b065853b74f19a69af9ad41815394ea
Felix Wessendorf <felix.wessendorf@naytec.de>
2022-07-25 17:20:25 +0200

    Ankündigung für Feuerwehrfest 2022

--- src/js/home-page.ctrl.js
+++ src/js/home-page.ctrl.js
@@ -2,13 +2,22 @@
 
     'use strict';
 
-    angular.module('app').controller('HomePageController',['$scope','ApiService',function($scope,apiService){
+    angular.module('app').controller('HomePageController', ['$scope', 'ApiService', '$uibModal', controller]);
 
-        $scope.apiEndpointUrl = function(){
+    function controller($scope, apiService, $uibModal) {
+
+        $scope.apiEndpointUrl = function() {
             return apiService.apiEndpointUrl();
         };
 
-    }]);
+        $uibModal.open({
+            animation: true,
+            templateUrl: 'templates/party-modal.html',
+            controller: 'ImageModalController',
+            scope: $scope,
+            size: 'lg'
+        });
 
+    }
 
 })();
