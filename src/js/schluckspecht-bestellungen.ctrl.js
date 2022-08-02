be11572910f0a81bb7faf3017ea6455e8ba570b8
Torben Oestreich <torben.oestreich@naytec.de>
2022-08-02 12:18:31 +0200

    Handy optimiert

--- src/js/schluckspecht-bestellungen.ctrl.js
+++ src/js/schluckspecht-bestellungen.ctrl.js
@@ -4,37 +4,73 @@
 
     angular.module('app').controller('SchluckspechtBestellungenController',['$scope','ApiService',function($scope, apiService){
 
+        $scope.passwordEntrie = '';
         $scope.count = 0;
+        $scope.success = false;
+        $scope.passwordSuccess = false;
+
+        $scope.passwordcheck = function (entry){
+            if (entry === 'test') {
+                $scope.passwordSuccess = true;
+            } else {
+                $scope.passwordSuccess = false;
+            }
+        };
 
         apiService.team.listAll()
             .then(function(response) {
                 $scope.teams = response
-                console.log(response);
             })
             .catch(function(error) {
                 console.log(error);
             });
 
-        $scope.buchen = function (id, amount) {
-            console.log(id, amount);
-            // apiService.team.book(id, amount)
-            //     .then(function(response) {
-            //         console.log(response);
-            //         $scope.count = 0;
-            //     })
-            //     .catch(function(error) {
-            //         console.log(error);
-            //     });
-            $scope.count = 0;
-            console.log($scope.teams)
-            $scope.teams.map(function (team){
+        $scope.book = function (id, amount) {
+            $scope.teams.map(function (team) {
                 if (team.id === id) {
                     $scope.teamname = team.name;
                 }
             })
 
-            $scope.success = true;
-            $scope.number = amount;
+            if (amount > 0) {
+                apiService.team.book(id, amount)
+                    .then(function() {
+                        $scope.booking = true;
+                        $scope.message = true;
+                        $scope.successmessage = amount + ' Getränke auf Team ' + $scope.teamname + ' gebucht';
+                        $scope.lastid = id;
+                        $scope.lastamount = amount;
+                        $scope.count = 0;
+                    })
+                    .catch(function(error) {
+                        $scope.successmessage = error[0].message;
+                        $scope.message = true;
+                    });
+            } else {
+                $scope.message = true;
+                $scope.successmessage = 'Bitte geben Sie eine Anzahl Getränke ein';
+                $scope.booking = false;
+                $scope.count = 0;
+            }
+        }
+
+        $scope.resetBooking = function() {
+            var deleteAmount = $scope.lastamount * -1;
+            apiService.team.book($scope.lastid, deleteAmount)
+                .then(function(response) {
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+            $scope.teams.map(function (team){
+                if (team.id === $scope.lastid) {
+                    $scope.teamname = team.name;
+                }
+            })
+            $scope.message = true;
+            $scope.successmessage = 'Letzte Buchung rückgängig gemacht ('+ deleteAmount +' Getränke ' + $scope.teamname +')';
+            $scope.booking = false;
+            $scope.count = 0;
         }
     }]);
 })();
