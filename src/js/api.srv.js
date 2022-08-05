8f884327b727685ce04cb63239bb8ec99abfcf35
TorbenNaytec <110100299+TorbenNaytec@users.noreply.github.com>
2022-08-05 09:37:11 +0200

    Schluckspecht buchen (#8)
    * Bestellungs Seite angelegt
    
    * Handy optimiert
    
    * css aufgeräumt
    
    * css aufgeräumt
    
    * bugfixing

--- src/js/api.srv.js
+++ src/js/api.srv.js
@@ -43,6 +43,12 @@
         this.team = {
             create: function(name, members) {
                 return call('Team', 'Create', [name, members]);
+            },
+            listAll: function(id, name) {
+                return call('Team', 'listAll', []);
+            },
+            book: function(id, amount) {
+                return call('Team', 'book', [id, amount]);
             }
         };
 
