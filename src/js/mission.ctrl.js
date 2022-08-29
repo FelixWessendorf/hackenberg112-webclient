2b2e74cfdeb43127acf22a74d1932e62b27d25f5
Torben Oestreich <torben.oestreich@naytec.de>
2022-08-29 09:55:12 +0200

    fertig

--- src/js/mission.ctrl.js
+++ src/js/mission.ctrl.js
@@ -7,16 +7,16 @@
         $scope.action = '';
         $scope.actionOthers = '';
         $scope.others = false;
-        $scope.FormSubmit = function (e) {
+        $scope.FormSubmit = function () {
             $scope.error = [];
             if($scope.date === null || $scope.date.length === 0) {
                 $scope.errorDate = true;
             }
-            if($scope.action === null || $scope.action.length === 0) {
+            if($scope.action === null || $scope.action.trim().length === 0) {
                 $scope.errorAction = true;
             }
             if ($scope.action === 'others') {
-                $scope.errorAction = $scope.actionOthers.length === 0;
+                $scope.errorAction = $scope.actionOthers.trim().length === 0;
             }
             if (!$scope.errorAction && !$scope.errorDate) {
                 console.log($scope.date);
@@ -24,11 +24,22 @@
                 console.log($scope.actionOthers);
             }
         }
+
         $scope.change = function () {
             $scope.others = $scope.action === 'others';
             $scope.errorAction = false;
         }
+
+        $scope.operationsDescription = [];
+        apiService.operation.loadOperations().then(function(operations){
+            $scope.operations = operations;
+
+            _.each(operations,function(operation){
+                if (!$scope.operationsDescription.includes(operation.description.trim())){
+                    $scope.operationsDescription.push(operation.description.trim());
+                }
+            });
+            $scope.operationsDescription.sort()
+        });
     }]);
-
-
 })();
