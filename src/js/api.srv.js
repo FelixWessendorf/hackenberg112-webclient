0a0b4f2fafcfe13e730378baa83cb6c9b8438445
Torben Oestreich <torben.oestreich@naytec.de>
2022-08-31 09:44:36 +0200

    Seite Einsatz hinzufügen & Controller fertig

--- src/js/api.srv.js
+++ src/js/api.srv.js
@@ -4,7 +4,7 @@
 
     angular.module('app').provider('ApiService',function(){
 
-        var apiEndpointUrl = 'http://api.hackenberg112.de';
+        var apiEndpointUrl = 'http://localhost:7070';
 
         var httpConfig = {};
 
@@ -22,6 +22,9 @@
             },
             loadOperations: function(){
                 return call('Operation','LoadOperations');
+            },
+            newMission: function(date, description, password){
+                return call('Operation','newMission', [date, description, password]);
             }
         };
 
--- src/js/mission.ctrl.js
+++ src/js/mission.ctrl.js
@@ -3,12 +3,13 @@
     'use strict';
 
     angular.module('app').controller('MissionController',['$scope','ApiService',function($scope, apiService){
+        $scope.password = '';
         $scope.date = '';
         $scope.action = '';
         $scope.actionOthers = '';
         $scope.others = false;
+        $scope.success = false;
         $scope.FormSubmit = function () {
-            $scope.error = [];
             if($scope.date === null || $scope.date.length === 0) {
                 $scope.errorDate = true;
             }
@@ -18,22 +19,38 @@
             if ($scope.action === 'others') {
                 $scope.errorAction = $scope.actionOthers.trim().length === 0;
             }
-            if (!$scope.errorAction && !$scope.errorDate) {
-                console.log($scope.date);
-                console.log($scope.action);
-                console.log($scope.actionOthers);
+            if($scope.password === null || $scope.password.trim().length === 0) {
+                $scope.errorPassword = 'Bitte Passwort angeben';
+            }
+            if (!$scope.errorAction && !$scope.errorDate && !$scope.errorPassword) {
+                let action = $scope.action.trim();
+                if ($scope.action === 'others') {
+                    action = $scope.actionOthers.trim();
+                }
+
+                apiService.operation.newMission($scope.date, action, $scope.password)
+                    .then(function() {
+                        $scope.success = true;
+                    })
+                    .catch(function(error) {
+                        if (error[0].source === 'password') {
+                            $scope.errorPassword = 'Passwort falsch';
+                        } else {
+                            console.log(error);
+                        }
+                    });
             }
         }
 
+        // Eingabefeld für neuen Einsatz anzeigen
         $scope.change = function () {
             $scope.others = $scope.action === 'others';
             $scope.errorAction = false;
         }
 
+        // Laden der vorhandenen Einsätze als Vorschläge
         $scope.operationsDescription = [];
         apiService.operation.loadOperations().then(function(operations){
-            $scope.operations = operations;
-
             _.each(operations,function(operation){
                 if (!$scope.operationsDescription.includes(operation.description.trim())){
                     $scope.operationsDescription.push(operation.description.trim());
