90df14cb3fff94efbfa2eb90ad169c060745559b
Felix Wessendorf <felix.wessendorf@naytec.de>
2023-01-02 09:31:31 +0100

    Code-Style

--- src/js/operation.ctrl.js
+++ src/js/operation.ctrl.js
@@ -2,25 +2,27 @@
 
     'use strict';
 
-    angular.module('app').controller('OperationController',['$scope','ApiService',function($scope, apiService){
+    angular.module('app').controller('OperationController', ['$scope', 'ApiService', function ($scope, apiService) {
+
         $scope.password = '';
         $scope.date = '';
         $scope.action = '';
         $scope.actionOthers = '';
         $scope.others = false;
         $scope.success = false;
-        $scope.FormSubmit = function () {
-            if($scope.date === null || $scope.date.length === 0) {
+
+        $scope.FormSubmit = function() {
+            if ($scope.date === null || $scope.date.length === 0) {
                 $scope.errorDate = true;
             }
-            if($scope.action === null || $scope.action.trim().length === 0) {
+            if ($scope.action === null || $scope.action.trim().length === 0) {
                 $scope.errorAction = true;
             }
             if ($scope.action === 'others') {
                 $scope.errorAction = $scope.actionOthers.trim().length === 0;
             }
-            if($scope.password === null || $scope.password.trim().length === 0) {
-                $scope.errorPassword = 'Bitte Passwort angeben';
+            if ($scope.password === null || $scope.password.trim().length === 0) {
+                $scope.errorPassword = 'Bitte ein Passwort angeben';
             }
             if (!$scope.errorAction && !$scope.errorDate && !$scope.errorPassword) {
                 let action = $scope.action.trim();
@@ -28,7 +30,7 @@
                     action = $scope.actionOthers.trim();
                 }
                 let formattedDate =  $scope.date.getFullYear() + '-' +
-                    ($scope.date.getMonth()+1).toString().padStart(2, '0') + '-' +
+                    ($scope.date.getMonth() + 1).toString().padStart(2, '0') + '-' +
                     $scope.date.getDate().toString().padStart(2, '0');
                 apiService.operation.newOperation(formattedDate, action, $scope.password)
                     .then(function() {
@@ -44,17 +46,16 @@
             }
         }
 
-        // Eingabefeld für neuen Einsatz anzeigen
         $scope.change = function () {
             $scope.others = $scope.action === 'others';
             $scope.errorAction = false;
         }
 
-        // Laden der vorhandenen Einsätze als Vorschläge
         $scope.operationsDescription = [];
-        apiService.operation.loadOperationNames().then(function(operationNames){
+        apiService.operation.loadOperationNames().then(function(operationNames) {
             $scope.operationsDescription = operationNames;
-            $scope.operationsDescription.sort();
         });
+
     }]);
+
 })();
